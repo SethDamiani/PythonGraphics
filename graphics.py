1c5addf497c38eacfefebb5ce097351e60497a32
SethDamiani <sethdamiani@gmail.com>
2017-10-16 09:24:54 -0400

    Add chained methods and keyword arguments to graphics objects

--- graphics.py
+++ graphics.py
@@ -3,168 +3,7 @@ import os
 import matplotlib.path as mpl_path
 import numpy as np
 from math import sqrt, pow
-# graphics.py
-"""
-Simple object oriented graphics library
 
-The library is designed to make it very easy for novice programmers to
-experiment with computer graphics in an object oriented fashion. It is
-written by John Zelle for use with the book "Python Programming: An
-Introduction to Computer Science" (Franklin, Beedle & Associates).
-
-LICENSE: This is open-source software released under the terms of the
-GPL (http://www.gnu.org/licenses/gpl.html).
-
-PLATFORMS: The package is a wrapper around Tkinter and should run on
-any platform where Tkinter is available.
-
-INSTALLATION: Put this file somewhere where Python can see it.
-
-OVERVIEW: There are two kinds of objects in the library. The GraphWin
-class implements a window where drawing can be done and various
-GraphicsObjects are provided that can be drawn into a GraphWin. As a
-simple example, here is a complete program to draw a circle of radius
-10 centered in a 100x100 window:
-
---------------------------------------------------------------------
-from graphics import *
-
-def main():
-    win = GraphWin("My Circle", 100, 100)
-    c = Circle(Point(50,50), 10)
-    c.draw(win)
-    win.getMouse() # Pause to view result
-    win.close()    # Close window when done
-
-main()
---------------------------------------------------------------------
-GraphWin objects support coordinate transformation through the
-setCoords method and pointer-based input through getMouse.
-
-The library provides the following graphical objects:
-    Point
-    Line
-    Circle
-    Oval
-    Rectangle
-    Polygon
-    Text
-    Entry (for text-based input)
-    Image
-
-Various attributes of graphical objects can be set such as
-outline-color, fill-color and line-width. Graphical objects also
-support moving and hiding for animation effects.
-
-The library also provides a very simple class for pixel-based image
-manipulation, Pixmap. A pixmap can be loaded from a file and displayed
-using an Image object. Both getPixel and setPixel methods are provided
-for manipulating the image.
-
-DOCUMENTATION: For complete documentation, see Chapter 4 of "Python
-Programming: An Introduction to Computer Science" by John Zelle,
-published by Franklin, Beedle & Associates.  Also see
-http://mcsp.wartburg.edu/zelle/python for a quick reference
-"""
-
-__version__ = "5.0"
-
-# Version 5.0 8/26/2016
-#     * update at bottom to fix MacOS issue causing askopenfile() to hang
-#     * update takes an optional parameter specifying update rate
-#     * Entry objects get focus when drawn
-#     * __repr_ for all objects
-#     * fixed offset problem in window, made canvas borderless
-#
-# Version 4.3 4/25/2014
-#     * Fixed Image getPixel to work with Python 3.4, TK 8.6 (tuple type handling)
-#     * Added interactive keyboard input (getKey and checkKey) to GraphWin
-#     * Modified setCoords to cause redraw of current objects, thus
-#       changing the view. This supports scrolling around via setCoords.
-# Version 4.2 5/26/2011
-#     * Modified Image to allow multiple undraws like other GraphicsObjects
-# Version 4.1 12/29/2009
-#     * Merged Pixmap and Image class. Old Pixmap removed, use Image.
-# Version 4.0.1 10/08/2009
-#     * Modified the autoflush on GraphWin to default to True
-#     * Autoflush check on close, setBackground
-#     * Fixed getMouse to flush pending clicks at entry
-# Version 4.0 08/2009
-#     * Reverted to non-threaded version. The advantages (robustness,
-#         efficiency, ability to use with other Tk code, etc.) outweigh
-#         the disadvantage that interactive use with IDLE is slightly more
-#         cumbersome.
-#     * Modified to run in either Python 2.x or 3.x (same file).
-#     * Added Image.getPixmap()
-#     * Added update() -- stand alone function to cause any pending
-#           graphics changes to display.
-#
-# Version 3.4 10/16/07
-#     Fixed GraphicsError to avoid "exploded" error messages.
-# Version 3.3 8/8/06
-#     Added checkMouse method to GraphWin
-# Version 3.2.3
-#     Fixed error in Polygon init spotted by Andrew Harrington
-#     Fixed improper threading in Image constructor
-# Version 3.2.2 5/30/05
-#     Cleaned up handling of exceptions in Tk thread. The graphics package
-#     now raises an exception if attempt is made to communicate with
-#     a dead Tk thread.
-# Version 3.2.1 5/22/05
-#     Added shutdown function for tk thread to eliminate race-condition
-#        error "chatter" when main thread terminates
-#     Renamed various private globals with _
-# Version 3.2 5/4/05
-#     Added Pixmap object for simple image manipulation.
-# Version 3.1 4/13/05
-#     Improved the Tk thread communication so that most Tk calls
-#        do not have to wait for synchronization with the Tk thread.
-#        (see _tkCall and _tkExec)
-# Version 3.0 12/30/04
-#     Implemented Tk event loop in separate thread. Should now work
-#        interactively with IDLE. Undocumented autoflush feature is
-#        no longer necessary. Its default is now False (off). It may
-#        be removed in a future version.
-#     Better handling of errors regarding operations on windows that
-#       have been closed.
-#     Addition of an isClosed method to GraphWindow class.
-
-# Version 2.2 8/26/04
-#     Fixed cloning bug reported by Joseph Oldham.
-#     Now implements deep copy of config info.
-# Version 2.1 1/15/04
-#     Added autoflush option to GraphWin. When True (default) updates on
-#        the window are done after each action. This makes some graphics
-#        intensive programs sluggish. Turning off autoflush causes updates
-#        to happen during idle periods or when flush is called.
-# Version 2.0
-#     Updated Documentation
-#     Made Polygon accept a list of Points in constructor
-#     Made all drawing functions call TK update for easier animations
-#          and to make the overall package work better with
-#          Python 2.3 and IDLE 1.0 under Windows (still some issues).
-#     Removed vestigial turtle graphics.
-#     Added ability to configure font for Entry objects (analogous to Text)
-#     Added setTextColor for Text as an alias of setFill
-#     Changed to class-style exceptions
-#     Fixed cloning of Text objects
-
-# Version 1.6
-#     Fixed Entry so StringVar uses _root as master, solves weird
-#            interaction with shell in Idle
-#     Fixed bug in setCoords. X and Y coordinates can increase in
-#           "non-intuitive" direction.
-#     Tweaked wm_protocol so window is not resizable and kill box closes.
-
-# Version 1.5
-#     Fixed bug in Entry. Can now define entry before creating a
-#     GraphWin. All GraphWins are now toplevel windows and share
-#     a fixed root (called _root).
-
-# Version 1.4
-#     Fixed Garbage collection of Tkinter images bug.
-#     Added ability to set text attributes.
-#     Added Entry boxes.
 
 try:  # import as appropriate for 2.x vs. 3.x
     import tkinter as tk
@@ -178,6 +17,7 @@ except:
 class GraphicsError(Exception):
     """Generic error class for graphics module exceptions."""
     pass
+
 
 OBJ_ALREADY_DRAWN = "Object currently drawn"
 UNSUPPORTED_METHOD = "Object doesn't support operation"
@@ -516,14 +356,17 @@ class GraphicsObject:
     def setFill(self, color):
         """Set interior color to color"""
         self._reconfig("fill", color)
+        return self
         
     def setOutline(self, color):
         """Set outline color to color"""
         self._reconfig("outline", color)
+        return self
         
     def setWidth(self, width):
         """Set line weight to width"""
         self._reconfig("width", width)
+        return self
 
     def draw(self, graphwin):
 
@@ -557,6 +400,7 @@ class GraphicsObject:
                 _root.update()
         self.canvas = None
         self.id = None
+        return self
 
     def move(self, dx, dy):
 
@@ -576,6 +420,7 @@ class GraphicsObject:
             self.canvas.move(self.id, x, y)
             if canvas.autoflush:
                 _root.update()
+        return self
            
     def _reconfig(self, option, setting):
         # Internal method for changing configuration of the object
@@ -703,8 +548,15 @@ class _BBox(GraphicsObject):
 
 class Rectangle(_BBox):
     
-    def __init__(self, p1, p2):
+    def __init__(self, p1, p2, **kwargs):
         _BBox.__init__(self, p1, p2)
+        for key in kwargs:
+            if key == "fill":
+                self.setFill(kwargs[key])
+            elif key == "outline":
+                self.setOutline(kwargs[key])
+            elif key == "width":
+                self.setWidth(kwargs[key])
 
     def __repr__(self):
         return "Rectangle({}, {})".format(str(self.p1), str(self.p2))
@@ -724,8 +576,15 @@ class Rectangle(_BBox):
 
 class Oval(_BBox):
     
-    def __init__(self, p1, p2):
+    def __init__(self, p1, p2, **kwargs):
         _BBox.__init__(self, p1, p2)
+        for key in kwargs:
+            if key == "fill":
+                self.setFill(kwargs[key])
+            elif key == "outline":
+                self.setOutline(kwargs[key])
+            elif key == "width":
+                self.setWidth(kwargs[key])
 
     def __repr__(self):
         return "Oval({}, {})".format(str(self.p1), str(self.p2))
@@ -745,11 +604,18 @@ class Oval(_BBox):
 
 class Circle(Oval):
     
-    def __init__(self, center, radius):
+    def __init__(self, center, radius, **kwargs):
         p1 = Point(center.x-radius, center.y-radius)
         p2 = Point(center.x+radius, center.y+radius)
         Oval.__init__(self, p1, p2)
         self.radius = radius
+        for key in kwargs:
+            if key == "fill":
+                self.setFill(kwargs[key])
+            elif key == "outline":
+                self.setOutline(kwargs[key])
+            elif key == "width":
+                self.setWidth(kwargs[key])
 
     def __repr__(self):
         return "Circle({}, {})".format(str(self.getCenter()), str(self.radius))
@@ -773,10 +639,19 @@ class Circle(Oval):
 
 class Line(_BBox):
     
-    def __init__(self, p1, p2):
+    def __init__(self, p1, p2, **kwargs):
         _BBox.__init__(self, p1, p2, ["arrow", "fill", "width"])
         self.setFill(DEFAULT_CONFIG['outline'])
         self.setOutline = self.setFill
+        for key in kwargs:
+            if key == "fill":
+                self.setFill(kwargs[key])
+            elif key == "outline":
+                self.setOutline(kwargs[key])
+            elif key == "width":
+                self.setWidth(kwargs[key])
+            elif key == "arrow":
+                self.setArrow(kwargs[key])
 
     def __repr__(self):
         return "Line({}, {})".format(str(self.p1), str(self.p2))
@@ -797,6 +672,7 @@ class Line(_BBox):
         if option not in ["first", "last", "both", "none"]:
             raise GraphicsError(BAD_OPTION)
         self._reconfig("arrow", option)
+        return self
 
     def containsPoint(self, p):
         if p is None:
@@ -808,12 +684,19 @@ class Line(_BBox):
 
 class Polygon(GraphicsObject):
     
-    def __init__(self, *points):
+    def __init__(self, *points, **kwargs):
         # if points passed as a list, extract it
         if len(points) == 1 and type(points[0]) == type([]):
             points = points[0]
         self.points = list(map(Point.clone, points))
         GraphicsObject.__init__(self, ["outline", "width", "fill"])
+        for key in kwargs:
+            if key == "fill":
+                self.setFill(kwargs[key])
+            elif key == "outline":
+                self.setOutline(kwargs[key])
+            elif key == "width":
+                self.setWidth(kwargs[key])
 
     def __repr__(self):
         return "Polygon" + str(tuple(p for p in self.points))
@@ -853,12 +736,25 @@ class Polygon(GraphicsObject):
 
 class Text(GraphicsObject):
     
-    def __init__(self, p, text):
+    def __init__(self, p, text, **kwargs):
         GraphicsObject.__init__(self, ["justify", "fill", "text", "font"])
         self.setText(text)
         self.anchor = p.clone()
         self.setFill(DEFAULT_CONFIG['outline'])
         self.setOutline = self.setFill
+        for key in kwargs:
+            if key == "fill":
+                self.setFill(kwargs[key])
+            elif key == "outline":
+                self.setOutline(kwargs[key])
+            elif key == "width":
+                self.setWidth(kwargs[key])
+            elif key == "text":
+                self.setText(kwargs[key])
+            elif key == "face":
+                self.setFace(kwargs[key])
+            elif key == "size":
+                self.setSize(kwargs[key])
 
     def __repr__(self):
         return "Text({}, '{}')".format(self.anchor, self.getText())
@@ -878,6 +774,7 @@ class Text(GraphicsObject):
 
     def setText(self, text):
         self._reconfig("text", text)
+        return self
         
     def getText(self):
         return self.config["text"]
@@ -891,6 +788,7 @@ class Text(GraphicsObject):
             self._reconfig("font", (face, s, b))
         else:
             raise GraphicsError(BAD_OPTION)
+        return self
 
     def setSize(self, size):
         if 5 <= size <= 150:
@@ -898,6 +796,7 @@ class Text(GraphicsObject):
             self._reconfig("font", (f, size, b))
         else:
             raise GraphicsError(BAD_OPTION)
+        return self
 
     def setStyle(self, style):
         if style in ['bold', 'normal', 'italic', 'bold italic']:
@@ -905,14 +804,16 @@ class Text(GraphicsObject):
             self._reconfig("font", (f, s, style))
         else:
             raise GraphicsError(BAD_OPTION)
+        return self
 
     def setTextColor(self, color):
         self.setFill(color)
+        return self
 
 
 class Entry(GraphicsObject):
 
-    def __init__(self, p, width):
+    def __init__(self, p, width, **kwargs):
         GraphicsObject.__init__(self, [])
         self.anchor = p.clone()
         # print self.anchor
@@ -923,6 +824,21 @@ class Entry(GraphicsObject):
         self.color = "black"
         self.font = DEFAULT_CONFIG['font']
         self.entry = None
+        for key in kwargs:
+            if key == "fill":
+                self.setFill(kwargs[key])
+            elif key == "outline":
+                self.setOutline(kwargs[key])
+            elif key == "width":
+                self.setWidth(kwargs[key])
+            elif key == "text":
+                self.setText(kwargs[key])
+            elif key == "face":
+                self.setFace(kwargs[key])
+            elif key == "size":
+                self.setSize(kwargs[key])
+            elif key == "style":
+                self.setStyle(kwargs[key])
 
     def __repr__(self):
         return "Entry({}, {})".format(self.anchor, self.width)
@@ -961,11 +877,13 @@ class Entry(GraphicsObject):
 
     def setText(self, t):
         self.text.set(t)
+        return self
             
     def setFill(self, color):
         self.fill = color
         if self.entry:
             self.entry.config(bg=color)
+        return self
 
     def _setFontComponent(self, which, value):
         font = list(self.font)
@@ -979,23 +897,27 @@ class Entry(GraphicsObject):
             self._setFontComponent(0, face)
         else:
             raise GraphicsError(BAD_OPTION)
+        return self
 
     def setSize(self, size):
         if 5 <= size <= 36:
             self._setFontComponent(1, size)
         else:
             raise GraphicsError(BAD_OPTION)
+        return self
 
     def setStyle(self, style):
         if style in ['bold', 'normal', 'italic', 'bold italic']:
             self._setFontComponent(2, style)
         else:
             raise GraphicsError(BAD_OPTION)
+        return self
 
     def setTextColor(self, color):
         self.color = color
         if self.entry:
             self.entry.config(fg=color)
+        return self
 
 
 class Image(GraphicsObject):
@@ -1003,7 +925,7 @@ class Image(GraphicsObject):
     idCount = 0
     imageCache = {}  # tk photoimages go here to avoid GC while drawn
     
-    def __init__(self, p, *pixmap):
+    def __init__(self, p, *pixmap, **kwargs):
         GraphicsObject.__init__(self, [])
         self.anchor = p.clone()
         self.imageId = Image.idCount
@@ -1013,6 +935,13 @@ class Image(GraphicsObject):
         else:  # width and height provided
             width, height = pixmap
             self.img = tk.PhotoImage(master=_root, width=width, height=height)
+        for key in kwargs:
+            if key == "fill":
+                self.setFill(kwargs[key])
+            elif key == "outline":
+                self.setOutline(kwargs[key])
+            elif key == "width":
+                self.setWidth(kwargs[key])
 
     def __repr__(self):
         return "Image({}, {}, {})".format(self.anchor, self.getWidth(), self.getHeight())
@@ -1070,6 +999,7 @@ class Image(GraphicsObject):
         
         """
         self.img.put("{" + color + "}", (x, y))
+        return self
 
     def save(self, filename):
         """Saves the pixmap image to filename.
